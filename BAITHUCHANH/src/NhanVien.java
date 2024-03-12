8ccbb0d2826fb49cb7501df92304c8e355f1ea6e
NhatMinh11 <nhatminhhh2k4@gmail.com>
2024-03-12 22:28:34 +0700

    Bai Tap

---
+++ BAITHUCHANH/src/NhanVien.java
@@ -0,0 +1,40 @@
+ import java.util.Scanner;
+
+public class NhanVien {
+ public static void main(String[] args){
+    String hoTen;
+    String GioiTinh;
+    String chucVu;
+    public int namSinh, tuoi;
+    public void nhaphoten(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ho Ten: ");
+        hoTen = sc.nextLine();
+    }
+    public void nhapgioitinh(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Gioi Tinh: ");
+        GioiTinh = sc.nextLine();
+    }
+    public void nhapchucvu(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Chuc Vu: ");
+        chucVu = sc.nextLine();
+    }
+    public void nhapnamsinh(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nam Sinh: ");
+        namSinh = sc.nextInt();
+    }
+    public void tinhTuoi(){
+        tuoi = 2024 - namSinh;
+    }
+    public void inthongtin(){
+        System.out.println("Ho Ten: " + hoTen);
+        System.out.println("Gioi Tinh: " + GioiTinh);
+        System.out.println("Chuc Vu: " + chucVu);
+        System.out.println("Tuoi: " + tuoi);
+    }
+}
+}
+
---
+++ BAITHUCHANH/src/QuanLySinhVien.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class QuanLySinhVien {
+public static void main(String[] args){
+    String hoTen;
+    String MSV;
+    String Namsinh;
+    String GioiTinh;
+    public void nhaphoten(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ho ten: ");
+        hoTen = sc.nextLine();
+    }
+    public void nhapMSV(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("MSV: ");
+        MSV = sc.nextLine();
+    }
+    public void nhapGioiTinh(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap gioi tinh: ");
+        GioiTinh = sc.nextLine();
+    }
+    void inThongTin(){
+        System.out.println("Ho Ten: " + hoTen);
+        System.out.println("MSV: " + MSV);
+        System.out.println("Gioi Tinh: " + GioiTinh);
+    }
+  }
+}
+
