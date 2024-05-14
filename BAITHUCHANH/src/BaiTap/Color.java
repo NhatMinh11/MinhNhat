02001994f4ae16f1b05f93b347707e366aea16bd
NhatMinh11 <nhatminhhh2k4@gmail.com>
2024-05-14 12:40:11 +0700

    baitap

---
+++ BAITHUCHANH/src/BaiTap/Color.java
@@ -0,0 +1,18 @@
+package BaiTap;
+
+import java.util.ArrayList;
+
+public class Color{
+    public static void main(String[] args){
+        ArrayList<String> colors = new ArrayList<String>(3);
+    colors.add("red");
+    colors.add("blue");
+    colors.add("green");
+    colors.add("orange");
+    colors.add("pink");
+    colors.add("yellow");
+    colors.remove("yellow");
+    System.out.println(colors.size());
+    System.out.println(colors);
+   }
+}
---
+++ BAITHUCHANH/src/MaxArr.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+import java.util.Scanner;
+
+public class MaxArr {
+    public static void main(String[] args){
+        ArrayList<Integer> arrList = new ArrayList<Integer>();
+        Scanner sc = new Scanner(System.in);
+        int number;
+        System.out.println("Nhap So Phan Tu Cua Mang : ");
+        int n = sc.nextInt(); 
+
+        for(int i = 0 ;i < n ; i++){
+            System.out.println("Nhap Phan Tu Thu " + i + ":");
+            number = sc.nextInt;
+            arrList.add(number);
+        } 
+        
+        int max = arrList.get(0);
+        for(int i = 1; i < arrList.size() ; i++){
+            if(arrList.get(i).compareTo(max) > 0){
+                max = arrList.get(i);
+            }
+        }
+        System.out.println("Phan Tu Lon NhatTrong Mang La : " +max);
+    }
+}
