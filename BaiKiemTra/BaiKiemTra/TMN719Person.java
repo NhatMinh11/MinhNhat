dd893dfd86f11eac9f1b685aa7f51ccd1f80b80a
NhatMinh11 <nhatminhhh2k4@gmail.com>
2024-05-22 10:08:17 +0700

    TMN719Cau2

--- BaiKiemTra/BaiKiemTra/TMN719Person.java
+++ BaiKiemTra/BaiKiemTra/TMN719Person.java
@@ -2,21 +2,25 @@ import java.util.Scanner;
 
 public class TMN719Person {
     public static void main(String[] args){
+       public class Person{ 
         public int tuoi;
         public float chieucao;
         public  double cannang;
+       }
         public void nhapthongtin(){
             Scanner sc = new Scanner(System.in);
             System.out.println("Nhap thong tin doi tuong:");
-            sc.tuoiInt();
-            sc.chieucaoFloat();
-            sc.cannangDouble();
+            tuoi = sc.nextInt();
+            chieucao = sc.nextFloat();
+            cannang = sc.nextDouble();
         }
         public void xuatThongtin(){
             System.out.println("Tuoi la:" +tuoi);
             System.out.println("Chieu cao la" +cannang);
             System.out.println("Chieu cao la :" +cannang);
         }
+
+        }
     }
-}
+
     
